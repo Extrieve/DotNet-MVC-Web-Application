32d3e906163198b5baa8820623aec3e73261e441
Nick <nicolas.cinera@gmail.com>
2021-10-06 21:57:14 -0400

    Fictional Character Quote Application

---
+++ Models/Quote.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string QuoteCharacter { get; set; }
+
+        public string QuoteAnswer { get; set; }
+
+        public Quote()
+        {
+
+        }
+    }
+}
